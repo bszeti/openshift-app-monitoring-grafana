4c5086ff72ad615406a19162abded8f35b47deb0
Balazs Szeti <szeti.balazs@gmail.com>
2022-09-29 18:11:50 -0400

    Restructure dirs

--- install.sh
+++ install.sh
@@ -4,20 +4,22 @@ oc apply -k kustomize/env/openshift-monitoring
 # Create grafana-monitoring namespace and install Grafana and operator
 oc apply -k kustomize/env/grafana-monitoring-operator
 
+# Create Grafana instance in grafana-monitoring namespace
 SATOKEN=`oc sa get-token grafana-thanos -n grafana-monitoring`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/grafana-monitoring/kustomization.yaml
 oc apply -k kustomize/env/grafana-monitoring
 
-# Create sso-1 namespace and install Grafana and SSO operator
-oc apply -k kustomize/env/sso-1-operator
 
-SATOKEN=`oc sa get-token grafana-thanos -n sso-1`
-sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/sso-1/kustomization.yaml
-oc apply -k kustomize/env/sso-1
+# Create team-a namespace and install Grafana and SSO operator
+oc apply -k kustomize/env/team-a-operators
 
-# Create sso-2 namespace and install Grafana and SSO operator
-oc apply -k kustomize/env/sso-2-operator
+SATOKEN=`oc sa get-token grafana-thanos -n team-a`
+sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-a/kustomization.yaml
+oc apply -k kustomize/env/team-a
 
-SATOKEN=`oc sa get-token grafana-thanos -n sso-2`
-sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/sso-2/kustomization.yaml
-oc apply -k kustomize/env/sso-2
+# Create team-b namespace and install Grafana and SSO operator
+oc apply -k kustomize/env/team-b-operator
+
+SATOKEN=`oc sa get-token grafana-thanos -n team-b`
+sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-b/kustomization.yaml
+oc apply -k kustomize/env/team-b
