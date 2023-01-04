d340b13b96a5577842c74095205a690a17b2aa1c
csfreak <github@csfreak.com>
2023-01-04 15:46:27 -0600

    job updates

--- kustomize/base/grafana/instance/sa-token-datasource-patch.sh
+++ kustomize/base/grafana/instance/sa-token-datasource-patch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+
+if [ -e /var/run/secrets/tokens/thanos-token]; then
+    TOKEN=$(cat /var/run/secrets/tokens/thanos-token)
+fi
+
 PATCH="[{\"op\": \"replace\", \"path\":\"/spec/datasources/0/secureJsonData/httpHeaderValue1\", \"value\": \"Bearer ${TOKEN}\"}]"
 
 set +x
