94516dde7114f085569e6dd23316811eb6e1fa33
Balazs Szeti <szeti.balazs@gmail.com>
2023-01-04 13:43:20 -0500

    Wait for CRDs

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ oc apply -k kustomize/env/openshift-monitoring
 
 # Create grafana-monitoring namespace and install Grafana operator
 oc apply -k kustomize/env/grafana-monitoring-operator
+oc wait --for condition=established crd/grafanas.integreatly.org
 
 # Create Grafana instance in grafana-monitoring namespace
 SATOKEN=`oc sa get-token grafana-thanos -n grafana-monitoring`
@@ -17,8 +18,10 @@ oc apply -k kustomize/env/team-b-namespace
 
 # Optional: Switch to user1
 
-# Create team-a namespace and install Grafana and SSO operator
+# Create team-a namespace and install Grafana, AMQ and SSO operator
 oc apply -k kustomize/env/team-a-operators
+oc wait --for condition=established crd/activemqartemises.broker.amq.io
+oc wait --for condition=established crd/keycloaks.keycloak.org
 
 SATOKEN=`oc sa get-token grafana-thanos -n team-a`
 # Use this in OCP v4.11 instead
@@ -28,7 +31,7 @@ oc apply -k kustomize/env/team-a-workload
 
 # Optional: Switch to user2
 
-# Create team-b namespace and install Grafana and SSO operator
+# Create team-b namespace and install Grafana, AMQ and SSO operator
 oc apply -k kustomize/env/team-b-operators
 
 SATOKEN=`oc sa get-token grafana-thanos -n team-b`
