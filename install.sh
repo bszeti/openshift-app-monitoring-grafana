d73e992075279282819d248a852ed3d7f8378812
Balazs Szeti <szeti.balazs@gmail.com>
2023-01-04 23:13:04 -0500

    Add http-https-echo to alerting

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 # Enable User Workload Monitoring - https://docs.openshift.com/container-platform/4.11/monitoring/enabling-monitoring-for-user-defined-projects.html
 oc apply -k kustomize/env/openshift-monitoring
 
+### Grafana
+
 # Create grafana-monitoring namespace and install Grafana operator
 oc apply -k kustomize/env/grafana-monitoring-operator
 while ! oc wait --for condition=established crd/grafanas.integreatly.org; do sleep 1; done
@@ -40,3 +42,19 @@ SATOKEN=`oc sa get-token grafana-thanos -n team-b`
 # SATOKEN=`oc create token grafana-thanos --duration=9000h -n team-b`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-b-workload/kustomization.yaml
 oc apply -k kustomize/env/team-b-workload
+
+
+### Alerts
+
+# Optional: Switch back to cluster-admin
+
+# Enable alert routing for user-defined alerts
+oc apply -k kustomize/env/openshift-monitoring-alerts
+
+# Optional: Switch to user1
+
+oc apply -k kustomize/env/team-a-alerts
+
+# Optional: Switch to user2
+
+oc apply -k kustomize/env/team-b-alerts
