c2661fab5b5246b9e087efa7f96a2f2321d7015e
Balazs Szeti <szeti.balazs@gmail.com>
2023-01-05 19:35:43 -0500

    tested on 4.11

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Enable User Workload Monitoring - https://docs.openshift.com/container-platform/4.11/monitoring/enabling-monitoring-for-user-defined-projects.html
 oc apply -k kustomize/env/openshift-monitoring
 
-### Grafana
+### Grafana ###
 
 # Create grafana-monitoring namespace and install Grafana operator
 oc apply -k kustomize/env/grafana-monitoring-operator
@@ -19,15 +19,15 @@ oc apply -k kustomize/env/grafana-monitoring
 oc apply -k kustomize/env/team-a-namespace
 oc apply -k kustomize/env/team-b-namespace
 
-# Optional: Switch to user1
+
+# Team-a - Optional: Switch to user1
 
 # Create team-a namespace and install Grafana, AMQ and SSO operator
 oc apply -k kustomize/env/team-a-operators
 
-# The first time you want to wait for CRDs to become ready on the cluster
+# The first time you want to wait for CRDs to become ready on the cluster - Only works as cluster-admin
 while ! oc wait --for condition=established crd/activemqartemises.broker.amq.io; do sleep 1; done
 while ! oc wait --for condition=established crd/keycloaks.keycloak.org; do sleep 1; done
-
 
 SATOKEN=`oc extract secret/grafana-thanos-token -n team-a --keys=token --to=-`
 # SATOKEN=`oc sa get-token grafana-thanos -n team-a`
@@ -36,7 +36,8 @@ SATOKEN=`oc extract secret/grafana-thanos-token -n team-a --keys=token --to=-`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-a-workload/kustomization.yaml
 oc apply -k kustomize/env/team-a-workload
 
-# Optional: Switch to user2
+
+# Team-b - Optional: Switch to user2
 
 # Create team-b namespace and install Grafana, AMQ and SSO operator
 oc apply -k kustomize/env/team-b-operators
@@ -49,7 +50,8 @@ sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-b-workload/kustomiza
 oc apply -k kustomize/env/team-b-workload
 
 
-### Alerts
+
+### Alerts ###
 
 # Optional: Switch back to cluster-admin
 
@@ -58,10 +60,18 @@ oc apply -k kustomize/env/openshift-monitoring-alerts
 
 # Optional: Switch to user1
 
+# AlertmanagerConfig
 oc apply -k kustomize/env/team-a-alerts
+
+# Send message to DLQ to trigger alert
+while ! oc wait -n team-a --for condition=Ready --timeout=180s pod/broker-ss-0; do sleep 1; done
 oc exec -n team-a broker-ss-0 -- sh -c '/home/jboss/amq-broker/bin/artemis producer --message-count 1 --destination DLQ --url tcp://$(hostname):61617 --user admin --password admin'
 
 # Optional: Switch to user2
 
+# AlertmanagerConfig
 oc apply -k kustomize/env/team-b-alerts
+
+# Send message to DLQ to trigger alert
+while ! oc wait -n team-b --for condition=Ready --timeout=180s pod/broker-ss-0; do sleep 1; done
 oc exec -n team-b broker-ss-0 -- sh -c '/home/jboss/amq-broker/bin/artemis producer --message-count 1 --destination DLQ --url tcp://$(hostname):61617 --user admin --password admin'
