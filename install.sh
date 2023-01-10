61b05e96c7bb827f0ccd399dd33404dfe1a9d26d
Balazs Szeti <szeti.balazs@gmail.com>
2023-01-10 10:19:56 -0500

    restructure for cluster scoped operators

--- install.sh
+++ install.sh
@@ -1,64 +1,53 @@
-# Enable User Workload Monitoring - https://docs.openshift.com/container-platform/4.11/monitoring/enabling-monitoring-for-user-defined-projects.html
+### Enable User Workload Monitoring - https://docs.openshift.com/container-platform/4.11/monitoring/enabling-monitoring-for-user-defined-projects.html
+# Run as cluster-admin
 oc apply -k kustomize/env/openshift-monitoring
 
-### Grafana ###
 
-# Create grafana-monitoring namespace and install Grafana operator
-oc apply -k kustomize/env/grafana-monitoring-operator
+### Operators ###
+oc apply -k kustomize/env/operator-grafana
+oc apply -k kustomize/env/operator-amq
+oc apply -k kustomize/env/operator-sso
+# The first time you want to wait for CRDs to become ready on the cluster
 while ! oc wait --for condition=established crd/grafanas.integreatly.org; do sleep 1; done
+while ! oc wait --for condition=established crd/activemqartemises.broker.amq.io; do sleep 1; done
+while ! oc wait --for condition=established crd/keycloaks.keycloak.org; do sleep 1; done
 
+
+### Cluster scoped Grafana ###
 # Create Grafana instance in grafana-monitoring namespace
-# SATOKEN=`oc sa get-token grafana-thanos -n grafana-monitoring`
 SATOKEN=`oc extract secret/grafana-thanos-token -n grafana-monitoring --keys=token --to=-`
-# Use this in OCP v4.11 instead
-# SATOKEN=`oc create token grafana-thanos --duration=9000h -n grafana-monitoring`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/grafana-monitoring/kustomization.yaml
 oc apply -k kustomize/env/grafana-monitoring
+
+
+### Team specific namespaces ### 
 
 # Create team namespaces - as cluster-admin
 oc apply -k kustomize/env/team-a-namespace
 oc apply -k kustomize/env/team-b-namespace
 
-
-# Team-a - Optional: Switch to user1
-
-# Create team-a namespace and install Grafana, AMQ and SSO operator
-oc apply -k kustomize/env/team-a-operators
-
-# The first time you want to wait for CRDs to become ready on the cluster - Only works as cluster-admin
-while ! oc wait --for condition=established crd/activemqartemises.broker.amq.io; do sleep 1; done
-while ! oc wait --for condition=established crd/keycloaks.keycloak.org; do sleep 1; done
-
+# Team-a - Optional: Run as user1
+# Grafana
 SATOKEN=`oc extract secret/grafana-thanos-token -n team-a --keys=token --to=-`
-# SATOKEN=`oc sa get-token grafana-thanos -n team-a`
-# Use this in OCP v4.11 instead
-# SATOKEN=`oc create token grafana-thanos --duration=9000h -n team-a`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-a-workload/kustomization.yaml
+# Workloads
 oc apply -k kustomize/env/team-a-workload
 
-
-# Team-b - Optional: Switch to user2
-
-# Create team-b namespace and install Grafana, AMQ and SSO operator
-oc apply -k kustomize/env/team-b-operators
-
+# Team-b - Optional: Run as user2
+# Grafana
 SATOKEN=`oc extract secret/grafana-thanos-token -n team-b --keys=token --to=-`
-# SATOKEN=`oc sa get-token grafana-thanos -n team-b`
-# Use this in OCP v4.11 instead
-# SATOKEN=`oc create token grafana-thanos --duration=9000h -n team-b`
 sed -i '' "s/Bearer .*/Bearer $SATOKEN/" kustomize/env/team-b-workload/kustomization.yaml
+# Workloads
 oc apply -k kustomize/env/team-b-workload
-
 
 
 ### Alerts ###
 
-# Optional: Switch back to cluster-admin
-
+# Run as cluster-admin
 # Enable alert routing for user-defined alerts
 oc apply -k kustomize/env/openshift-monitoring-alerts
 
-# Optional: Switch to user1
+# Optional: Run as user1
 
 # AlertmanagerConfig
 oc apply -k kustomize/env/team-a-alerts
@@ -69,7 +58,7 @@ oc exec -n team-a broker-ss-0 -- sh -c '/home/jboss/amq-broker/bin/artemis produ
 while ! oc wait -n team-a --for condition=Ready --timeout=180s pod/broker-ss-1; do sleep 1; done
 oc exec -n team-a broker-ss-1 -- sh -c '/home/jboss/amq-broker/bin/artemis producer --message-count 1 --destination DLQ --url tcp://$(hostname):61617 --user admin --password admin'
 
-# Optional: Switch to user2
+# Optional: Run as user2
 
 # AlertmanagerConfig
 oc apply -k kustomize/env/team-b-alerts
